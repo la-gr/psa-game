2336007a373088fe4798db6a88f05cb0283efa8c
lara grahek <lara@grahek.net>
2025-05-26 12:22:09 -0400

    hands

--- psa/blackMar.gd
+++ psa/blackMar.gd
@@ -1,2 +1,21 @@
 extends Node2D
 #black market
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:	
+	var hand1 = get_node("hand1")
+	print("hello")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	hands()
+
+func hands() -> void:
+	print("hands")
+	
+func _input(event):
+	var hand2 = get_node("hand2");
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		hand2.position(event.position)
+		print(event.position)
--- psa/node_2d.gd
+++ psa/node_2d.gd
@@ -1,1 +1,28 @@
 extends Node2D
+#black market
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:	
+	var hand1 = get_node("hand1")
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	hands()
+
+func hands() -> void:
+	var hand1 = 2
+	
+func _input(event):
+	var hand2 = get_node("hand2");
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		hand2.set_frame(1)
+		var tween = create_tween()
+		tween.tween_property(hand2, "position", event.position, 0.2)
+		#hand2.position=event.position
+		await get_tree().create_timer(0.5).timeout #waits
+		hand2.set_frame(0)
+		await get_tree().create_timer(0.2).timeout #waits
+		tween.kill() # Abort the previous animation.
+		tween = create_tween()
+		tween.tween_property(hand2, "position", Vector2(891.0, 342.0), 0.2)
