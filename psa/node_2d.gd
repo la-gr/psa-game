8b7649031299debcbaf285260fa577ae0d8ecfa7
lara grahek <lara@grahek.net>
2025-06-03 13:24:13 -0400

    added lives and descriptions of items

--- psa/node_2d.gd
+++ psa/node_2d.gd
@@ -3,7 +3,7 @@ extends Node2D
 var tween
 var num = 1
 var hand1
-var items = ["Cyber", "dog"]
+var items = ["Cyber", "dog", "flour", "Cat", "House", "Couch", 'Tree', "foot", "worm", "shoe", "worm2", "man", "Baby pic", "Phone", "cheese", "baby"]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	var hand2 = get_node("hand2")
@@ -28,12 +28,18 @@ func _input(event):
 		hand1.set_frame(0)
 		await get_tree().create_timer(0.2).timeout #waits
 		var l = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-		#print(l)
 		if (!l):
 			tween.kill() # Abort the previous animation.
 			tween = create_tween()
 			tween.tween_property(hand1, "position", Vector2(843.0, 421.0), 0.2)
 
+func life_lost() -> void:
+	var lives = get_node("lives")
+	var o = lives.get_frame()
+	if (o!=0):
+		lives.set_frame(o-1)
+	else:
+		pass #end game
 		
 func but_click(n) -> void:
 	num=n
@@ -42,16 +48,6 @@ func but_click(n) -> void:
 	pop_up.set_visible(true)
 	pop_up.set_frame(n)
 
-
-func _on_button_pressed() -> void:
-	but_click(0)
-
-func _on_button_2_pressed() -> void:
-	but_click(1)
-
-func _on_button_3_pressed() -> void:
-	but_click(0)
-
 func _on_pop_but_pressed() -> void:
 	#var p = items[num]
 	var group = get_node("scroll/FlowContainer/"+items[num])
@@ -59,3 +55,36 @@ func _on_pop_but_pressed() -> void:
 	await get_tree().create_timer(0.7).timeout
 	group.set_visible(false)
 	pop_up.set_visible(false)
+
+func _on_button_pressed() -> void:
+	but_click(0)
+func _on_button_2_pressed() -> void:
+	but_click(1)
+func _on_button_3_pressed() -> void:
+	but_click(2)
+func _on_button_4_pressed() -> void:
+	but_click(3)
+func _on_button_5_pressed() -> void:
+	but_click(4)
+func _on_button_6_pressed() -> void:
+	but_click(5)
+func _on_button_7_pressed() -> void:
+	but_click(6)
+func _on_button_8_pressed() -> void:
+	but_click(7)
+func _on_button_9_pressed() -> void:
+	but_click(8)
+func _on_button_10_pressed() -> void:
+	but_click(9)
+func _on_button_11_pressed() -> void:
+	but_click(10)
+func _on_button_12_pressed() -> void:
+	but_click(11)
+func _on_button_13_pressed() -> void:
+	but_click(12)
+func _on_button_14_pressed() -> void:
+	but_click(13)
+func _on_button_15_pressed() -> void:
+	but_click(14)
+func _on_button_16_pressed() -> void:
+	but_click(15)
