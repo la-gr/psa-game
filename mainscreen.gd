75129a931c01be505fcaf651f584bef81a14041b
mlewoo <152655011+mlewoo@users.noreply.github.com>
2025-06-08 16:37:12 -0400

    reupload mainscreen

---
+++ mainscreen.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var play = get_node("Play") 
+	var options = get_node("Options")
+	var help = get_node("Help")
+	var exit = get_node("Exit")
+	var lvlSelect = get_node("Level Select")
+	var optionsMenu = get_node("OptionsMenu")
+	var helpPanel = get_node("HelpPanel")
+	
+	lvlSelect.set_visible(false)
+	optionsMenu.set_visible(false)
+	helpPanel.set_visible(false)
+	
+
+#called when play button is pressed
+func _on_play_pressed() -> void:
+	var lvlSelect = get_node("Level Select")
+	lvlSelect.set_visible(true); 
+
+#called when options button is pressed
+func _on_options_pressed() -> void:
+	var optionsMenu = get_node("OptionsMenu")
+	optionsMenu.set_visible(true)
+
+
+#called when help button is pressed
+func _on_help_pressed() -> void:
+	var helpPanel = get_node("HelpPanel")
+	helpPanel.set_visible(true)
+
+#called when exit button is pressed
+func _on_exit_pressed() -> void:
+	get_tree().quit() 
+
+#blackmarket episode is pressed
+func _on_black_market_pressed() -> void:
+	get_tree().change_scene_to_file("res://blackMarket.tscn") 
+
+#close level selection
+func _on_exit_levels_pressed() -> void:
+	var lvlSelect = get_node("Level Select")
+	lvlSelect.set_visible(false)
+
+
+func _on_exit_options_pressed() -> void:
+	var optionsMenu = get_node("OptionsMenu")
+	optionsMenu.set_visible(false)
+
+
+func _on_exit_help_pressed() -> void:
+	var helpPanel = get_node("HelpPanel")
+	helpPanel.set_visible(false)
